bc434583f8dfd55e1b0445b8369c21b081976e4b
Andy Watson <werdnamac@gmail.com>
2022-09-05 13:07:22 -0400

    Initial commit

--- app.js
+++ app.js
@@ -1,13 +1,66 @@
+const button = document.getElementById("btn");
 
     // Create Dino Constructor
-
+    class Dinosaur {
+    constructor(species, weight, height, diet, where, when, fact) {
+        this.species = species;
+        this.weight = weight;
+        this.height = height;
+        this.diet = diet;
+        this.where = where;
+        this.when = when;
+        this.fact = fact;
+    }
+}
 
     // Create Dino Objects
+    // populate an array with dino data from the json file
+    let dinoData = [];
+    fetch('dino.json')
+    .then(res => res.json())
+    .then(data => {
+        dinoData = data.Dinos.map(dino =>
+            new Dinosaur(dino))
+    })
+    console.log(dinoData);
+    
 
+    // Create Human Class
 
-    // Create Human Object
+    class Human {
+        constructor(height, weight, diet) {
+            this.height = height;
+            this.weight = weight;
+            this.diet = diet;
+        }
+    }
 
-    // Use IIFE to get human data from form
+    // Use IIFE to get human data from form and create a human object
+    const humanData = (
+        function() {
+            let name = getInputValue("name");
+            let heightFeet = ParseFloat((getInputValue("feet")));
+            let heightInches = ParseFloat((getInputValue("inches")));
+            let height = heightFeet + heightInches;
+            let diet = getInputValue("diet");
+
+            return function () {
+                currentHuman = new Human(this.name, this.height, this.diet);
+                console.log(name);
+            }
+        }
+    )();
+
+        // when user clicks on the button, a human object with all the 
+        // info in the form should be created.
+        button.addEventListener("click", () => {
+            console.log("clicked");
+            humanData();
+            console.log("clicked");
+        })
+
+    console.log(currentHuman);
+    
 
 
     // Create Dino Compare Method 1
@@ -30,3 +83,4 @@
 
 
 // On button click, prepare and display infographic
+
